6c7de8e27ce2c14b79a0021550b123d1aadf6ba2
Pericles ADJOVI <55035365+Pericles001@users.noreply.github.com>
2021-04-05 22:55:29 +0100

    onClick event done with onClick ebvent handler

--- src/components/KeyPadComponent.js
+++ src/components/KeyPadComponent.js
@@ -1,20 +1,74 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class KeyPadComponent extends Component{
-  render(){
-    return(
-   
-<div className="button">
-<button>(</button>
-<button>CE</button>
-<button>)</button>
-<button>C</button>
+class KeyPadComponent extends Component {
+  render() {
+    return (
+      <div className="button">
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="(">
+          (
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="CE">
+          CE
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name=")">
+          )
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="C">
+          C
+        </button>
 
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="1">
+          1
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="2">
+          2
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="3">
+          3
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="+">
+          +
+        </button>
 
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="4">
+          4
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="5">
+          5
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="6">
+          6
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="-">
+          -
+        </button>
 
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="7">
+          7
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="8">
+          8
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="9">
+          9
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="*">
+          *
+        </button>
 
-
-</div>
-      );
+        <button onClick={(e) => this.props.onClick(e.target.name)} name=".">
+          .
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="0">
+          0
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="=">
+          =
+        </button>
+        <button onClick={(e) => this.props.onClick(e.target.name)} name="/">
+          /
+        </button>
+      </div>
+    );
   }
 }
