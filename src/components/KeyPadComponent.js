67d16fb5ff92d6d81795479607ee896936e40176
Pericles ADJOVI <55035365+Pericles001@users.noreply.github.com>
2021-04-05 20:35:17 +0100

    calculator buttons started to create input elements

---
+++ src/components/KeyPadComponent.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class KeyPadComponent extends Component{
+  render(){
+    return(
+   
+<div className="button">
+<button>(</button>
+<button>CE</button>
+<button>)</button>
+<button>C</button>
+
+
+
+
+
+</div>
+      );
+  }
+}
